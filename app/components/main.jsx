840f52fe496390ad2a13344de7ee0a72a4cc0ac4
Luke Rees <escrew7@hotmail.com>
2017-06-20 06:18:48 +0800

    add countdown functionality

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -6,7 +6,7 @@ var Main = (props) => {
   return (
     <div>
               <Nav/>
-      <div classNAme="row">
+      <div className="row">
         <div className="column small-centered medium-6 large-4">
             {props.children}
         </div>
